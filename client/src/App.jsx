fe8ccc0be15fff970ef78cc38e4297a3293068f2
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-06-15 00:12:55 +0530

    feat: implement Login page layout and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import LoginPage from "../assets/images/loginPage.png";
+import { FaUserCircle } from "react-icons/fa";
 
 const Login = () => {
   return (
     <>
-    <div>Login</div>
+      <div className="max-w-6xl h-[90vh] flex justify-self-center items-center">
+        <div className="grid grid-cols-2 gap-10">
+          <div className="bg-(--background) flex justify-center rounded-2xl  items-center">
+            <img src={LoginPage} alt="" />
+          </div>
+          <form className="flex flex-column gap-2 rounded-xl shadow-xl bg-(--card-background) ">
+            <div>
+              <FaUserCircle />
+            </div>
+          </form>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

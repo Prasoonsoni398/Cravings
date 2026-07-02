7b0704aa1c7957429b701f32573ebeac805b1bf6
Prasoonsoni398 <soniprasoon395@gmail.com>
2026-07-02 23:21:16 +0530

    feat: Implement Sidebar component in UserDashboard; add dynamic menu items and user profile display

--- client/src/components/userDashboard/Sidebar.jsx
+++ client/src/components/userDashboard/Sidebar.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import {
+  MdOutlineDashboard,
+  MdOutlineBorderColor,
+  MdFavoriteBorder,
+} from "react-icons/md";
+import { PiListHeart } from "react-icons/pi";
+import { BsPersonGear } from "react-icons/bs";
+import { useAuth } from "../../context/AuthContext";
 
-const Sidebar = () => {
+const MenuItems = [
+  { name: "Overview", icon: <MdOutlineDashboard /> },
+  { name: "Orders", icon: <MdOutlineBorderColor /> },
+  { name: "WishList", icon: <PiListHeart /> },
+  { name: "Setting", icon: <BsPersonGear /> },
+];
+
+const Sidebar = ({ active, setActive }) => {
+  const { user } = useAuth();
   return (
     <>
-    <div>Sidebar</div>
-    </>
-  )
-}
+      <div className=" p-2">
+        <div className="flex gap-3 items-center border-b-2 border-primary p-2 pb-4 ">
+          <div className="w-10 h-10 rounded-full overflow-hidden cursor-pointer">
+            <img
+              src={user.photo}
+              alt=""
+              className="w-full h-full object-cover "
+            />
+          </div>
+          <h1 className="text-xl font-semibold flex flex-col cursor-pointer">
+            <span className="text-warning">{user.fullName}</span>
+          </h1>
+        </div>
 
-export default Sidebar
+        <div className="space-y-2 mt-4 p-3">
+          {MenuItems.map((item, idx) => (
+            <button
+              key={idx}
+              className={`flex gap-3 px-4 font-semibold items-center  rounded-lg w-full p-2 ${active === item.name ? "bg-primary text-primary-content" : "hover:bg-primary/10"}`}
+              onClick={() => setActive(item.name)}
+            >
+              {item.icon}
+              <span>{item.name}</span>
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
--- client/src/pages/Dashboard/UserDashboard.jsx
+++ client/src/pages/Dashboard/UserDashboard.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
+import Sidebar from "../../components/userDashboard/Sidebar";
+import Orders from "../../components/userDashboard/Order";
+import WishList from "../../components/userDashboard/WishList";
+import Settings from "../../components/userDashboard/Settings";
+import Overview from "../../components/userDashboard/Overview";
 
 const UserDashboard = () => {
   const { user } = useAuth();
+  const [active , setActive]= useState("Overview")
   return (
     <>
-      <div className="flex gap-3">
-        <div>Sidebar</div>
-        <div>Content</div>
+     <div className="flex h-[91vh]">
+        <div className="w-1/6 border border-primary h-full">
+          <Sidebar active={active} setActive={setActive} />
+        </div>
+        <div className="w-5/6 border border-warning h-full">
+          {active === "Overview" && <Overview />}
+          {active === "Orders" && <Orders />}
+          {active === "WishList" && <WishList />}
+          {active === "Setting" && <Settings />}
+        </div>
       </div>
     </>
   );
